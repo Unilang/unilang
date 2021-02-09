4ce2caccee3f268f59a4a223e101bf2d82db470d
Trevor Hickey <thickey@uber.com>
2021-02-09 14:45:22 -0500

    updates

--- source/code/utilities/filesystem/files/creating/lib.cpp
+++ source/code/utilities/filesystem/files/creating/lib.cpp
@@ -1,8 +1,14 @@
 #include "code/utilities/filesystem/files/creating/lib.hpp"
 #include "code/utilities/filesystem/paths/lib.hpp"
 #include "code/utilities/types/strings/transformers/stripping/lib.hpp"
+#include "code/utilities/filesystem/files/observers/lstat_wrap/lib.hpp"
 #include <fstream>
 
+void Create_File_If_It_Doesnt_Exist(std::string const& path_to_file){
+  if (!File_Exists(path_to_file)){
+    Write_To_File(path_to_file,"");
+  }
+}
 void Create_File_Even_If_The_Path_Doesnt_Exist(std::string const& path_to_file){
   auto path = Get_File_Path_Without_The_Filename(path_to_file);
   Create_Path_If_It_Doesnt_Already_Exist(path);
