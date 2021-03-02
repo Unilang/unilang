0206eb8cd11472f2557a17e16866da8cc533a3b1
Trevor Hickey <thickey@uber.com>
2021-03-02 15:48:33 -0500

    updates

--- source/code/external_projects/bf/running/buildfarm_scripts/fix_redis.sh
+++ source/code/external_projects/bf/running/buildfarm_scripts/fix_redis.sh
@@ -5,7 +5,7 @@ sudo pkill -9 java
 sudo pkill -9 redis-ser
 sudo rm -rf /tmp/worker/*
 sudo rm -rf /tmp/gpu_worker/*
-#sudo /etc/init.d/redis-server stop 
+sudo /etc/init.d/redis-server stop 
 #sudo systemctl stop redis.service
 sudo rm -rf ~/Desktop/redis/utils/create-cluster/*.log
 sudo rm -rf ~/Desktop/redis/utils/create-cluster/*.aof
