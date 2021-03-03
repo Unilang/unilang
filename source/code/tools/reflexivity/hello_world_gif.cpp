2173f95831d21baf6de719ce299f2476b9cfe962
Trevor Hickey <thickey@uber.com>
2021-03-03 12:45:54 -0500

    updates

--- source/code/tools/reflexivity/hello_world_gif.cpp
+++ source/code/tools/reflexivity/hello_world_gif.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "code/utilities/code/concept_tree_to_image.hpp"
+#include "code/utilities/code/animate/unilang_hello_world_gif.hpp"
 #include "code/utilities/filesystem/paths/lib.hpp"
 
 
 int main(int argc, char** argv){
     
-    Concept_Tree_To_Image::Create();
+    Unilang_Hello_World_Gif::Create();
 }
