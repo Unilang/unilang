0f38b8dcf2a7a57ffaef28777011dda48ec4e334
Trevor Hickey <thickey@uber.com>
2021-03-26 12:38:42 -0400

    updates

--- source/code/hello_world/sh/test/hello_world_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-EXPECTED="Hello, World!"
-OUTPUT=$(./code/hello_world/sh/hello_world.sh)
-
-if [ "$OUTPUT" != "$EXPECTED" ]; then
-    echo "$OUTPUT != $EXPECTED"
-    exit 1
-fi
