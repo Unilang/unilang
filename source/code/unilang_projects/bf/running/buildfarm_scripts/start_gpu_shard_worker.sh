e4094dccab85a93c3d749f723956860d83e50aad
Trevor Hickey <thickey@uber.com>
2021-04-11 22:24:05 -0400

    updates

--- source/code/unilang_projects/bf/running/buildfarm_scripts/start_gpu_shard_worker.sh
+++ source/code/unilang_projects/bf/running/buildfarm_scripts/start_gpu_shard_worker.sh
@@ -4,5 +4,5 @@ move_to_buildfarm_repo
 
 sleep 4;
 printf "\e]2;GPU WORKER\a"
-bazelisk run //src/main/java/build/buildfarm:buildfarm-shard-worker ~/Desktop/unilang/source/code/external_projects/bf/running/configs/shard-gpu-worker.config.example
+bazelisk run //src/main/java/build/buildfarm:buildfarm-shard-worker ~/Desktop/unilang/source/code/unilang_projects/bf/running/configs/shard-gpu-worker.config.example
 read;
--- source/code/unilang_projects/bf/running/buildfarm_scripts/start_shard_worker.sh
+++ source/code/unilang_projects/bf/running/buildfarm_scripts/start_shard_worker.sh
@@ -4,5 +4,5 @@ move_to_buildfarm_repo
 
 sleep 4;
 printf "\e]2;CPU WORKER\a"
-bazelisk run //src/main/java/build/buildfarm:buildfarm-shard-worker ~/Desktop/unilang/source/code/external_projects/bf/running/configs/shard-cpu-worker.config.example
+bazelisk run //src/main/java/build/buildfarm:buildfarm-shard-worker ~/Desktop/unilang/source/code/unilang_projects/bf/running/configs/shard-cpu-worker.config.example
 read;
