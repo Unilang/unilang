5ced63ba0b1e32fa522a8f8d27bd99683aaa0880
Trevor Hickey <thickey@uber.com>
2021-05-27 13:07:52 -0400

    updates

--- source/code/utilities/crypto/bitcoin_wallet_generator_test.cpp
+++ source/code/utilities/crypto/bitcoin_wallet_generator_test.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
+#include "code/utilities/crypto/bitcoin_wallet_generator.hpp"
 
 TEST(TestGeneration, WordToWallet) {
-  EXPECT_EQ(2+2,4);
+  //EXPECT_EQ(Bitcoin_Wallet_Generator::Create("bitcoin").wallet_address,"18VkRiDhFu2Z17AvtpU3vL2LbTXDzCvDVo");
+  EXPECT_EQ(Bitcoin_Wallet_Generator::Create("bitcoin!").wallet_address,"19fQQ5FruqpmvbRzeEn6CHJHLvHxD59ph9");
 }
