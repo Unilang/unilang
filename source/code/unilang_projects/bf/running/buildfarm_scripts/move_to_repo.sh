687612c96dd5a334c3ef010b8f8b14b1edca906b
Trevor Hickey <thickey@uber.com>
2021-06-20 13:54:19 -0400

    updates

--- source/code/unilang_projects/bf/running/buildfarm_scripts/move_to_repo.sh
+++ source/code/unilang_projects/bf/running/buildfarm_scripts/move_to_repo.sh
@@ -2,7 +2,7 @@
 
 
 function move_to_buildfarm_repo {
-   #cd ~/Desktop/repos/bazel-buildfarm;
-   cd ~/Desktop/repos/buildfarm-fork;
+   cd ~/Desktop/repos/bazel-buildfarm;
+   #cd ~/Desktop/repos/buildfarm-fork;
    #cd /home/luxe/Desktop/tt/bazel-buildfarm-1;
 }
