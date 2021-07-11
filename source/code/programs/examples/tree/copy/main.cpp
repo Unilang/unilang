c7e28726505e8aba898111beaed87eaa1ffa1bf1
Trevor Hickey <thickey@uber.com>
2021-07-10 20:39:34 -0400

    updates

--- source/code/programs/examples/tree/copy/main.cpp
+++ source/code/programs/examples/tree/copy/main.cpp
@@ -1,9 +1,19 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
+#include <vector>
 #include "src/tree.hh"
 
 using namespace std;
+
+struct bigger_object {
+  int a;
+  int b;
+  int c;
+  std::string d;
+  std::vector<std::string> e;
+  tree<string> t;
+};
 
 tree<string> get_tree();
 
@@ -16,8 +26,16 @@ int main(int, char **)
   auto b = get_tree();
   a = b;
   
+  tree<string> c;
+  bigger_object object;
+  
+  auto t2 = get_tree();
+  object.t = t2;
+  
+  
   std::cout << a.size() << std::endl;
   std::cout << b.size() << std::endl;
+  std::cout << t2.size() << std::endl;
 }
 
 tree<string> get_tree() {
@@ -27,3 +45,8 @@ tree<string> get_tree() {
   tr.insert(top, "one");
   return tr;
 }
+
+
+
+
+
